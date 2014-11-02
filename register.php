c4751b6b2836b55d92601891a8d6e34424788200
harwooja <jharwood93@gmail.com>
2014-11-01 20:02:17 -0400

    fixed selection statement error

--- register.php
+++ register.php
@@ -1,5 +1,7 @@
 <?php
 $uname = $_POST['username'];
+$userfound = false;
+
 try{
 
 $m = new MongoClient(); // create a new mongo client
@@ -13,31 +15,27 @@ echo "Connected to the mongodb server";
 if (isset($_POST['username']) && isset($_POST['password'])) 
 {
     
-    $userfound = False;
-
     foreach($findUser as $user){
             $storedUser = $user["username"]; 
 
             if ($uname == $storedUser){
-                $userfound = True;
-                echo "There is already a user with this username";
+                $userfound = true;
                 break;
             }
         }
 
-    if ($usersfound == False){
+    if ($userfound == false){
     $document = array("username" => $_POST['username'],"password" => $_POST['password']);
     $collection->insert($document);
     header('Location: index.php');
 
+    } else if ($userfound == true){
+        echo "There is already a user with this username";
     }
 
 }
 
-
-
-} 
-catch (MongoConnectionException $e) {
+} catch (MongoConnectionException $e) {
   echo "Couldn't conect to the mongodb server";
 }
 
